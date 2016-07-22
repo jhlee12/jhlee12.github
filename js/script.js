bd0f1522bf90891ba4ab676d80b97640224f6393
jhlee12 <jeonghyun0925@gmail.com>
2016-07-21 23:31:00 -0400

    Update 07-21-2016

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ function openNav()
     document.getElementById("main").style.marginLeft = "250px";
     document.getElementById("main").style.backgroundColor = "rgba(0,0,0,2)";
     document.getElementById("main").style.opacity = 0.4;
+    document.getElementById("nav").style.display = "none";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
@@ -14,11 +15,24 @@ function closeNav()
     document.getElementById("main").style.marginLeft = "0";
     document.getElementById("main").style.opacity = 1;
     document.getElementById("main").style.backgroundColor = "rgba(0,0,0,0)";
+    document.getElementById("nav").style.display = "flex";
 }
 
-/* Fade in for header */
+function loadUrl()
+{
+	$(".about").fadeIn(4000);
+}
+
+
 $(document).ready(function() {
-	$(".header").fadeIn(4000);
-	$("#logo").fadeIn(4500);
-	$(".quote").delay(1000).fadeIn(3000);
+	/* Fade in for header */
+	$(".header").fadeIn(3000);
+	$("#logo").fadeIn(3500);
+	$(".quote").delay(500).fadeIn(3000);
+
+	$(window).resize(function() {
+		$(".nav").css("height", $(".openbtn").outerHeight())
+		$(".nav").css("max-height", $(".openbtn").outerHeight())
+	});
+	$(".about").fadeIn(4000);
 });
