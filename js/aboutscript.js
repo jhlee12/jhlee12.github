7a39b06b66ea8a45147031f125f70b7a721683da
jhlee12 <jhlee12@users.noreply.github.com>
2016-07-22 10:06:19 -0400

    Update aboutscript.js

--- js/aboutscript.js
+++ js/aboutscript.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".aboutme").fadeIn(4e3),$(".image").fadeIn(4e3),$(window).resize(function(){$(".nav").css("height",$(".openbtn").outerHeight(!0)),$(".nav").css("max-height",$(".openbtn").outerHeight(!0))})});
+$(document).ready(function(){$(".aboutme").fadeIn(2e3),$(".image").fadeIn(2e3),$(window).resize(function(){$(".nav").css("height",$(".openbtn").outerHeight(!0)),$(".nav").css("max-height",$(".openbtn").outerHeight(!0))})});
