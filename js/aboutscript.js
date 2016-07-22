09a5dff9935bf26ca9ae53acaf3afac531a11f3d
jhlee12 <jeonghyun0925@gmail.com>
2016-07-22 09:31:57 -0400

    Minor changes

--- js/aboutscript.js
+++ js/aboutscript.js
@@ -1,24 +1,3 @@
-function openNav()
-{
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.getElementById("main").style.backgroundColor = "rgba(0,0,0,2)";
-    document.getElementById("main").style.opacity = 0.4;
-    document.getElementById("nav").style.display = "none";
-}
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-function closeNav()
-{
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.getElementById("main").style.opacity = 1;
-    document.getElementById("main").style.backgroundColor = "rgba(0,0,0,0)";
-    document.getElementById("nav").style.display = "flex";
-}
-
-$(".about").height();
-
 $(document).ready(function() {
     $(".aboutme").fadeIn(4000);
     $(".image").fadeIn(4000);
