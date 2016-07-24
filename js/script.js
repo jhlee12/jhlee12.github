72839c702e8f7e19b3b1c53fc6eb43ad3215b0d2
jhlee12 <jeonghyun0925@gmail.com>
2016-07-24 15:33:12 -0400

    Update 07-24-2016

--- js/script.js
+++ js/script.js
@@ -1,1 +1,52 @@
-function openNav(){document.getElementById("mySidenav").style.width="250px",document.getElementById("main").style.marginLeft="250px",document.getElementById("main").style.backgroundColor="rgba(0,0,0,2)",document.getElementById("main").style.opacity=.4,document.getElementById("nav").style.display="none"}function closeNav(){document.getElementById("mySidenav").style.width="0",document.getElementById("main").style.marginLeft="0",document.getElementById("main").style.opacity=1,document.getElementById("main").style.backgroundColor="rgba(0,0,0,0)",document.getElementById("nav").style.display="flex"}$(document).ready(function(){$(".page_title").delay(1e3).fadeIn(1000)});
+$(document).ready(function() {
+    $(".page_title").delay(1000).fadeIn(1000);
+
+    $(".openbtn").click(function() {
+        if (window.innerWidth < 743.125)
+        {
+            $("#mySidenav").css("display", "block");
+            $("#mySidenav").animate({height: "225px"}).css("maxHeight","225px");
+            $("#main").animate({marginTop: "225px"}).css("backgroundColor", "rgba(0,0,0,2)").css("opacity", "0.4");
+        }
+        else
+        {
+            $("#mySidenav").css("display", "block");
+            $("#mySidenav").width("250px");
+            $("#main").css("marginLeft", "250px");
+        }
+        $("#nav").css("display", "none");
+        $("#main").css("opacity", "0.4").css("backgroundColor", "rgba(0,0,0,2)");
+    });
+
+    $(".closebtn, #main").click(function() {
+        if (window.innerWidth < 743.125)
+        {
+            $("#mySidenav").height(0).css("maxHeight", "0");
+            $("#main").animate({marginTop: "0"}, function() {document.getElementById("mySidenav").style.display = "none";});
+        }
+        else
+        {
+            $("#mySidenav").width(0);
+            $("#main").css("marginLeft", 0);
+            setTimeout(function() {document.getElementById("mySidenav").style.display = "none";}, 500);
+        }
+        $("#nav").css("display", "flex");
+        $("#main").css("opacity", 1).css("backgroundColor", "rgba(0,0,0,0)");
+    })
+
+    $(window).resize(function() {
+        if (document.getElementById("mySidenav").style.display == "block")
+        {
+            if (window.innerWidth < 743.125)
+            {
+                $("#mySidenav").css("minHeight", "0").css("width", "100%").css("maxHeight", "225px").height("225px");
+                $("#main").css("marginLeft", "0").css("marginTop", "225px");
+            }
+            else
+            {
+                $("#mySidenav").css("maxHeight", "100%").css("minHeight", "100%").height("100%").css("minWidth", "0").width("250px");
+                $("#main").css("marginLeft", "250px").css("marginTop", "0");
+            }
+        }
+    });
+});
