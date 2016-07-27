a75bc5f681a3af9dc46c1deee04b8d5ae68f63b3
Jeong Hyun Lee <jhlee12@users.noreply.github.com>
2016-07-27 09:33:25 -0400

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop() > $(window).height()){
-            $(".masthead").css({"background":"url(../img/triangular.png) repeat center center fixed"});   
-        }
-        else{
-            $(".masthead").css({"background-color:":"transparent"});
-        }
-
-    })
-})
+$(document).scroll(function() {
+	scroll_start = $(this).scrollTop();
+    if(scroll_start > (window.innerHeight - $(".masthead").outerHeight(true)))
+    	{
+    		$('.masthead').css('background-color', 'rgba(34,34,34,0.9)');
+       	}
+       	else
+       	{
+          $('.masthead').css('background-color', 'transparent');
+       	}
+});
