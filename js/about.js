e557f3f8e75e2b39a61ea08c795e4a2278e94353
jhlee12 <jeonghyun0925@gmail.com>
2016-08-20 23:07:41 -0400

    Major Update 08-20-2016

--- js/about.js
+++ js/about.js
@@ -1,70 +1,76 @@
 $(document).ready(function() {
-    $(".title-text").fadeIn(1000).delay(300).fadeOut(1000,function(){$(this).text("jeong hyun lee").fadeIn(1000)});
+    $('.title-text').fadeIn(1000).delay(300).fadeOut(1000,function() {
+        $(this).text('jeong hyun lee').fadeIn(1000);
+        $(this).attr('title', 'jeong hyun lee');
+    });
 
-    $("body").scrollspy({target: "#aboutNav", offset: $(".masthead").height() + 60});
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('body').scrollspy({target: '#aboutNav', offset: $('.masthead').height() + 60});
 
     var $window = $(window);
-    var $navbar = $(".masthead");
-    var $scrollTop = $(".scroll-to-top-btn, .about");
+    var $navbar = $('.masthead');
+    var $scrollTop = $('.scroll-to-top-btn, .about');
     if($navbar.length > 0)
     {
-        $window.on("scroll", function()
+        $window.on('scroll', function()
         {
             if ($(this).scrollTop() > window.innerHeight * 0.6)
             {
-                $navbar.addClass("stuck");
-                $(".nav1").removeClass("active");
-                $(".nav2").addClass("active");
+                $navbar.addClass('stuck');
+                $('.nav1').removeClass('active');
+                $('.nav2').addClass('active');
             }
             else
             {
-                $navbar.removeClass("stuck");
-                $(".nav2").removeClass("active");
-                $(".nav1").addClass("active");
+                $navbar.removeClass('stuck');
+                $('.nav2').removeClass('active');
+                $('.nav1').addClass('active');
             }
         });
     };
 
-    $window.on("scroll", function()
+    $window.on('scroll', function()
     {
         if ($(this).scrollTop() > window.innerHeight * 0.7)
         {
-            $(".scroll-to-top-btn").addClass("visible");
+            $('.scroll-to-top-btn').addClass('visible');
         }
         else
         {
-            $(".scroll-to-top-btn").removeClass("visible");
+            $('.scroll-to-top-btn').removeClass('visible');
         }
     });
 
-    $scrollTop.on("click", function(event) {
-        if (this.hash !== "")
+    $scrollTop.on('click', function(event) {
+        if (this.hash !== '')
         {
             event.preventDefault();
             var hash = this.hash;
-            $("html, body").animate({scrollTop: 0}, 800, function() {
+            $('html, body').animate({scrollTop: 0}, 800, function() {
                 window.location.hash = hash;
             });
         }
     });
 
-    $("a.page-scroll").on("click", function(event) {
-        if (this.hash !== "")
+    $('a.page-scroll').on('click', function(event) {
+        if (this.hash !== '')
         {
             event.preventDefault();
             var hash = this.hash;
-            $("html, body").animate({scrollTop: $(hash).offset().top - $navbar.height()}, 800, function() {
+            $('html, body').animate({scrollTop: $(hash).offset().top - $navbar.height()}, 800, function() {
                 window.location.hash = hash;
             });
         }
     });
 
-    $(".modal-frame").click(function() {
-        $("#myPicModal").css("display", "block");
-        $("#img").attr("src", $("img", this).attr("src"));
+    $('.modal-frame').click(function() {
+        $('#myPicModal').css('display', 'block');
+        var $img = $('img', this);
+        $('#img').attr('src', $img.attr('src')).attr('title', $img.attr('title'));
     });
 
-    $("#myPicModal").click(function() {
-        $("#myPicModal").css("display", "none");
+    $('#myPicModal').click(function() {
+        $('#myPicModal').css('display', 'none');
     });
 });
--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-    $(".quote").width(parseFloat(window.innerWidth)).height(parseFloat($(".text").css("height")));
+    $('.quote').width(parseFloat(window.innerWidth)).height(parseFloat($('.text').css('height')));
 
     /* Fade in for header */
-    $(".header").fadeIn(2000);
-    $("#logo").fadeIn(2500);
-    $(".text").delay(500).fadeIn(2000);
-    $(".quote").height(parseFloat($(".text").css("height")));
+    $('.header').fadeIn(2000);
+    $('#logo').fadeIn(2500);
+    $('.text').delay(500).fadeIn(2000);
+    $('.quote').height(parseFloat($('.text').css('height')));
 
-    $(".masthead-brand").delay(1300).fadeIn(1500);
+    $('.masthead-brand').delay(1300).fadeIn(1500);
 
     $(window).resize(function() {
-        $(".introduction, .quote").width(parseFloat(window.innerWidth));
+        $('.introduction, .quote').width(parseFloat(window.innerWidth));
     });
 });
